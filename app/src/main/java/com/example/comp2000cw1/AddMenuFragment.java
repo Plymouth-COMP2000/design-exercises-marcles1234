7565f3117bb97071a13780819e3c6d7046bb1de8
Marc Jones <marc.ioan55@gmail.com>
2025-12-22 19:56:32 +0000

    Data validation

--- app/src/main/java/com/example/comp2000cw1/AddMenuFragment.java
+++ app/src/main/java/com/example/comp2000cw1/AddMenuFragment.java
@@ -22,7 +22,8 @@ public class AddMenuFragment extends Fragment {
     public AddMenuFragment() {
         // Required empty public constructor
     }
-    
+
+
     public static AddMenuFragment newInstance(String param1, String param2) {
         AddMenuFragment fragment = new AddMenuFragment();
         Bundle args = new Bundle();
@@ -34,6 +35,7 @@ public class AddMenuFragment extends Fragment {
 
     EditText editDishName, editDishType, editDishDescription, editDishPrice, editDishAllergens, editDishImage;
     Button addButton;
+    boolean dishTypeValidMarker;
 
 
     @Override
@@ -48,13 +50,15 @@ public class AddMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_add_menu, container, false);
 
+        //BACK BUTTON
         View backButton = root.findViewById(R.id.back);
         backButton.setOnClickListener(v -> {
             ((MainActivity) requireActivity()).goBack();
         });
+
+        String[] categories = {"Starter", "Pasta", "Pizza", "Soup", "Sides"};
 
         editDishName=root.findViewById(R.id.dishName);
         editDishType=root.findViewById(R.id.dishType);
@@ -64,9 +68,11 @@ public class AddMenuFragment extends Fragment {
         editDishImage=root.findViewById(R.id.dishImage);
         addButton=root.findViewById(R.id.addMenuButton);
 
+        //ADD DISH BUTTON
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                dishTypeValidMarker = false;
                 DataModel data = new DataModel(
                         editDishName.getText().toString(),
                         editDishType.getText().toString(),
@@ -74,12 +80,28 @@ public class AddMenuFragment extends Fragment {
                         editDishPrice.getText().toString(),
                         editDishAllergens.getText().toString(),
                         editDishImage.getText().toString());
-                DatabaseHelper databaseHelper = new DatabaseHelper(requireContext());
-                boolean success = databaseHelper.addDish(data);
-                if(success) {
-                    Toast.makeText(requireContext(), "Dish added successfully", Toast.LENGTH_SHORT).show();
+
+                for (String category : categories) {
+                    if (editDishType.getText().toString().equals(category)) {;
+                        dishTypeValidMarker = true;
+                        break;
+                    }
+                }
+                if (!dishTypeValidMarker) {
+                    Toast.makeText(requireContext(), editDishType.getText().toString() + " is a invalid category", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(requireContext(), "Error adding dish", Toast.LENGTH_SHORT).show();
+                    DatabaseHelper databaseHelper = new DatabaseHelper(requireContext());
+                    DataModel existingDish = databaseHelper.getDishByName(data.getDishName());
+                    if (existingDish != null) {
+                        Toast.makeText(requireContext(), editDishName.getText().toString() + " already exists", Toast.LENGTH_SHORT).show();
+                    } else {
+                        boolean success = databaseHelper.addDish(data);
+                        if (success) {
+                            Toast.makeText(requireContext(), "Dish added successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(requireContext(), "Error adding dish", Toast.LENGTH_SHORT).show();
+                        }
+                    }
                 }
             }
 
--- app/src/main/java/com/example/comp2000cw1/editMenuFragment.java
+++ app/src/main/java/com/example/comp2000cw1/editMenuFragment.java
@@ -31,6 +31,8 @@ public class editMenuFragment extends Fragment {
     EditText editDishName, editDishType, editDishDescription, editDishPrice, editDishAllergens, editDishImage;
     Button updateMenuButton, deleteMenuButton;
 
+    boolean dishTypeValidMarker;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,6 +76,7 @@ public class editMenuFragment extends Fragment {
         editDishAllergens.setText(itemData.getDishAllergens());
         editDishImage.setText(itemData.getDishImage());
 
+        String[] categories = {"Starter", "Pasta", "Pizza", "Soup", "Sides"};
 
         updateMenuButton.setOnClickListener(v -> {
             DataModel data = new DataModel(
@@ -83,7 +86,24 @@ public class editMenuFragment extends Fragment {
                     editDishPrice.getText().toString(),
                     editDishAllergens.getText().toString(),
                     editDishImage.getText().toString());
-            databaseHelper.updateDish(dishName, data);
+
+            for (String category : categories) {
+                if (editDishType.getText().toString().equals(category)) {;
+                    dishTypeValidMarker = true;
+                    break;
+                }
+            }
+            if (!dishTypeValidMarker) {
+                Toast.makeText(requireContext(), editDishType.getText().toString() + " is a invalid category", Toast.LENGTH_SHORT).show();
+            } else {
+                DataModel existingDish = databaseHelper.getDishByName(data.getDishName());
+                if (existingDish != null) {
+                    Toast.makeText(requireContext(), editDishName.getText().toString() + " already exists", Toast.LENGTH_SHORT).show();
+                } else {
+                    databaseHelper.updateDish(dishName, data);
+                    Toast.makeText(requireContext(), "Dish updated successfully", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
 
         deleteMenuButton.setOnClickListener(v -> {
